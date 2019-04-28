1820ad0105dffe68ab9da4b19b3b62ce576be0ac
chth <chth668@163.com>
2019-04-28 14:28:48 +0800

    k8s脚本配置

--- springboot-springcloud-docker-kebernetes-front/src/main/java/springboot.springcloud.docker.kebernetes.front/FrontApplication.java
+++ springboot-springcloud-docker-kebernetes-front/src/main/java/springboot.springcloud.docker.kebernetes.front/FrontApplication.java
@@ -1,9 +1,16 @@
 package springboot.springcloud.docker.kebernetes.front;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 /**
  * @author: 程泰恒
@@ -12,8 +19,31 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-public class FrontApplication {
+public class FrontApplication implements ApplicationRunner {
+
+    @Autowired
+    private Environment env;
+
     public static void main(String[] args) {
         SpringApplication.run(FrontApplication.class, args);
     }
+
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("url-service"+env.getProperty("url.service"));
+    }
+
+    @Configuration
+    @PropertySource("classpath:/META-INF/url-dev.properties")
+    @Profile("dev")
+    public static class Dev{
+
+    }
+
+    @Configuration
+    @PropertySource("classpath:/META-INF/url-docker.properties")
+    @Profile("docker")
+    public static class Docker{
+
+    }
+
 }
