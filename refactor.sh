9187aa772d0109f0b4f34a954566e5d40aeeb236
chth <chth668@163.com>
2019-04-28 09:27:14 +0800

    脚本逻辑完善

--- refactor.sh
+++ refactor.sh
@@ -12,11 +12,12 @@ docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:l
 docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:latest  2>/dev/null 1>&2
 docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:latest 2>/dev/null 1>&2
 
-docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:latest
-docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:latest
-docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:latest
 
 #用pom文件打包项目，构建docker镜像
 mvn -f ./springboot-springcloud-docker-kebernetes-eureka/pom.xml clean package docker:build
 mvn -f ./springboot-springcloud-docker-kebernetes-front/pom.xml clean package docker:build
 mvn -f ./springboot-springcloud-docker-kebernetes-service/pom.xml clean package docker:build
+
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:latest
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:latest
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:latest
