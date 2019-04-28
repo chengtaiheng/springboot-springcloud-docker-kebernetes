3b8411eb8b1c4308b68ebd8c26b93419cbf99f1b
chth <chth668@163.com>
2019-04-28 09:21:52 +0800

    脚本完善

--- refactor.sh
+++ refactor.sh
@@ -4,9 +4,17 @@
 docker images | grep '<none>' | awk -F ' ' '{print$3}' | xargs docker rmi 2>/dev/null 1>&2
 
 #删除原有镜像
-docker rmi chth/springboot-springcloud-docker-kebernetes-eureka:1.0.0 2>/dev/null 1>&2
-docker rmi chth/springboot-springcloud-docker-kebernetes-front:1.0.0  2>/dev/null 1>&2
-docker rmi chth/springboot-springcloud-docker-kebernetes-service:1.0.0 2>/dev/null 1>&2
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:1.0.0 2>/dev/null 1>&2
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:1.0.0  2>/dev/null 1>&2
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:1.0.0 2>/dev/null 1>&2
+
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:latest 2>/dev/null 1>&2
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:latest  2>/dev/null 1>&2
+docker rmi 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:latest 2>/dev/null 1>&2
+
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-eureka:latest
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-front:latest
+docker push 192.168.99.115/chth/springboot-springcloud-docker-kebernetes-service:latest
 
 #用pom文件打包项目，构建docker镜像
 mvn -f ./springboot-springcloud-docker-kebernetes-eureka/pom.xml clean package docker:build
